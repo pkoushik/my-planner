356638b387979908d75e46bfb72335b8f6024982
pkoushik <pkoushik11@gmail.com>
2017-12-03 01:09:21 -0500

    able to create classes now

--- temp/app/modules/classes/controller.py
+++ temp/app/modules/classes/controller.py
@@ -4,13 +4,27 @@ import re
 import requests
 import subprocess
 from app.modules.auth.model import User
-from app.modules.classes.model import Class
+from app.modules.classes.model import Class, CreateClassForm
 #from app.modules.events.model import Event
-from flask import Blueprint, render_template, flash, request, redirect, url_for, jsonify
+from flask import Blueprint, render_template, flash, request, redirect, \
+    url_for, jsonify
 from flask_security import current_user, login_required
 from bson import json_util
 
 classes = Blueprint('classes', __name__)
+
+def filter_form(form):
+    """ Router for CRUD Forms that were Recieved on the Group Dashboard """
+    print("trying to filter")
+    if form['submit'] == 'create':
+        return add_class(form)
+    # elif form['submit'] == 'update':
+    #     return update_group(form)
+    # elif form['submit'] == 'delete':
+    #     return delete_group(form)
+
+    flash('error Could not Fulfill Request. Please Try Again.')
+    return redirect(url_for('classes.home'))
 
 @classes.route('/', methods=['GET', 'POST'])
 @login_required
@@ -19,7 +33,8 @@ def home():
     if request.method == 'POST':
         return filter_form(request.form)
     user = current_user._get_current_object()
-    return render_template('classes/dashboard.html', classes=user.classes, user=user)
+    form = CreateClassForm()
+    return render_template('classes/dashboard.html', classes=user.classes, user=user, form=form)
 
 @classes.route('/addClass', methods=['POST'])
 @login_required
@@ -27,22 +42,30 @@ def add_class(form=None):
     if form is None:
         flash('Invalid request to add a class')
 
-    add_class_form = AddClassForm() # form to add a class
+    add_class_form = CreateClassForm(form) # form to add a class
 
-    if not form.validate():
-    	flash('Invalid information was provided in creating a class. Please try again.')
-    	return redirect(request.args.get('next') or url_for('classes.home'))
+    if not add_class_form.validate():
+        print(add_class_form.name)
+        print(add_class_form.professor)
+        print(add_class_form.days)
+        flash('Invalid information was provided in creating a class. Please try again.')
+        return redirect(request.args.get('next') or url_for('classes.home'))
 
 
     # form is valid
     try:
-    	user = current_user._get_current_object()
+        user = current_user._get_current_object()
 
-    	# class that is created from the form
-    	current_class = Class(owner=user,name=add_class_form.name.data, person = add_class_form.person.data, start_time = add_class_form.start_time.data,
-    	 end_time = add_class_form.end_time.data, days = add_class_form.days.data, start_date = add_class_form.start_date.data, end_date = add_class_form.end_date.data)
+        # class that is created from the form
+        current_class = Class(owner=user, name=add_class_form.name.data, professor = add_class_form.professor.data, days = add_class_form.days.data).save()
 
-    	user.classes.append(current_class)
+        user.classes.append(current_class)
+        user.save()
+        valid_emails = ["idk"]
+
+        flash('success Added Class: {}'.format(current_class.name))
 
     except Exception as e:
-    	flash('error An Error has occurred, Please Try Again. {}'.format(e))
+        flash('error An Error has occured, Please Try Again. {}'.format(e))
+
+    return redirect(request.args.get('next') or url_for('classes.home'))
--- temp/app/modules/classes/model.py
+++ temp/app/modules/classes/model.py
@@ -3,12 +3,13 @@ from datetime import datetime as dt
 
 from app.modules.auth.model import User
 
-from wtforms import Form, validators, StringField, RadioField
+from wtforms import Form, validators, StringField, RadioField, \
+    SelectMultipleField, DateTimeField
 
 class Class(db.Document):
     owner = db.ReferenceField(User, required=True)
     name = db.StringField(required=True, min_length=3, max_length=50)
-    person = db.ReferenceField(User, required=True)
+    professor = db.ReferenceField(User, required=True)
     #events = db.ListField(db.ReferenceField(Events))
     start_time = db.DateTimeField(default=dt.now())
     end_time = db.DateTimeField(default=dt.now())
@@ -17,14 +18,15 @@ class Class(db.Document):
     end_date = db.DateTimeField(default=dt.now())
     meta = {'strict': False}
 
-class AddClassForm(Form):
-    name = StringField('Class Name', [validators.Length(min=1, max=100),
+class CreateClassForm(Form):
+    name = StringField('Class Name', [validators.Length(min=3, max=100),
                                         validators.DataRequired()])
-    person = StringField('Professor Name', [validators.Length(min=1, max=100),
-                                        validators.DataRequired()])
-    #start_time = DateTimeField(default=dt.now())
-    #end_time = DateTimeField(default=dt.now())
-    #days = SelectMultipleField('Days', choices=[
-    #    ('sunday',"Sunday"),('monday', 'Monday'), ('tuesday', 'Tuesday'), ('wednesday', "Wednesday"), ('thursday', "Thursday"), ('friday', "Friday"), ('saturday', "Saturday")])
-    #start_date = DateTimeField(default=dt.now())
-    #end_date = DateTimeField(default=dt.now())
+    professor = StringField('Professor Name', [validators.DataRequired()])
+    days = RadioField('Days of the Week', choices=[
+        ('sunday', 'Sunday'), ('monday', 'Monday'), ('tuesday', "Tuesday"),
+        ('wednesday', 'Wednesday'), ('thursday', 'Thursday'),
+        ('friday', "Friday")])
+    # start_date = DateTimeField('Start Date', format='%Y-%m-%d %H:%M:%S')
+    # end_date = DateTimeField('End Date', format='%Y-%m-%d %H:%M:%S')
+    # class_start_time = DateTimeField('Start Time', format='%Y-%m-%d %H:%M:%S')
+    # class_end_time = DateTimeField('End Time', format='%Y-%m-%d %H:%M:%S')
